5f22ad30c930a949ebc92935933dfbb8d01d4d33
FatimaZelay <fatima.elay@gmail.com>
2026-01-22 13:45:36 +0100

    feat: server.js #4 #5

---
+++ server.js
@@ -0,0 +1,37 @@
+
+// console.log('SERVER.JS IS RUNNING')
+
+import express from 'express'
+import { Liquid } from 'liquidjs'
+
+const app = express()
+const PORT = 3000
+
+// Static files (CSS, JS, images)
+app.use(express.static('public'))
+
+// Body parsing (forms)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+const engine = new Liquid({
+  root: './views',
+  extname: '.liquid'
+})
+
+app.engine('liquid', engine.express())
+app.set('view engine', 'liquid')
+app.set('views', './views')
+
+
+// HOME
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
